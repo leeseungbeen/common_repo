72c44919da02506f5cda0c853b84d164e42bdd88
zeroandi <zeroandi@doubleugames.com>
2020-12-03 14:12:49 +0900

    feat: mobile 구현체 추가

---
+++ TestJson/src/json/TiledToMobile.java
@@ -0,0 +1,11 @@
+package json;
+
+public class TiledToMobile implements IConvertingJson{
+
+	@Override
+	public void convertingJson(String path, String targetName, String resultName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
